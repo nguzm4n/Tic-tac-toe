95f77d61af1b34d3d10d00799d10d1423b7315be
Nicolás Guzmán <n3kguzman@gmail.com>
2024-04-02 17:01:08 -0300

    TIC TAC TOE

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './estilos/global.css'
 import Tablero from './componentes/tablero.jsx'
+import Jugadores from './componentes/jugadores.jsx'
 
 
 
 const App = () => {
+
+  const [jugador1, setJugador1] = useState("")
+  const [jugador2, setJugador2] = useState("")
+  const [turno, setTurno] = useState(null)
+
   return (
     <div className='container'>
       <h1>Tic Tac Toe in React.js</h1>
       <div className='container-juego'>
-        <Tablero />
+        {
+          jugador1 !== "" && jugador2 !== "" && turno !== null ? 
+          (
+            <Tablero />
+          ) : (
+            <Jugadores 
+            jugador1={jugador1}
+            jugador2={jugador2}
+            setJugador1={setJugador1}
+            setJugador2={setJugador2}
+            setTurno={setTurno}
+    
+             />
+          )
+        }
+       
+        
       </div>
 
     </div>
--- src/componentes/jugadores.jsx
+++ src/componentes/jugadores.jsx
@@ -2,26 +2,26 @@ import React from "react";
 import '../estilos/jugadores.css'
 
 
-function Jugadores({jugador1, jugador2}) {
+function Jugadores({ jugador1, jugador2, setJugador1, setJugador2, setTurno }) {
 
 
-    return(
+  return (
 
-        <div className="panel-jugadores">
-          <div className="cyw">
-            <h2>Choose Your Weapon</h2>
-            <div className="inputs">
-              <input placeholder="Player 1 Username" type="text"></input>
-              <input placeholder="Player 2 Username" type="text"></input>
-            </div>
-            <br></br>
-            <div className="botones">
-              <button className="boton-x">X</button>
-              <button className="boton-o">O</button>
-            </div>
-          </div>
+    <div className="panel-jugadores">
+      <div className="cyw">
+        <h2>Choose Your Weapon</h2>
+        <div className="inputs">
+          <input placeholder="Player 1 Username" type="text" value={jugador1} onChange={e => setJugador1(e.target.value)} />
+          <input placeholder="Player 2 Username" type="text" value={jugador2} onChange={e => setJugador2(e.target.value)} />
         </div>
-    )
+        <br></br>
+        <div className="botones">
+          <button className="boton-x botones5" onClick={() => setTurno('X')}>X</button>
+          <button className="boton-o botones5" onClick={() => setTurno('O')}>O</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Jugadores
